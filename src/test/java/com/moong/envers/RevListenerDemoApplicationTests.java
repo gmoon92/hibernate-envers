41a04f8213a2c6ee3d31ae5631f28e279a9009cb
gmoon92 <gmoon0929@gmail.com>
2019-11-07 17:14:40 +0900

    Initial commit

---
+++ src/test/java/com/moong/envers/RevListenerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moong.envers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevListenerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
