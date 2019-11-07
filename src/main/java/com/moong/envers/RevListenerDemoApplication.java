000fc3a5125e8eaf9dd896dc8b997afc5a286771
gmoon0929 <gmoon0929@gmail.com>
2019-11-07 17:14:40 +0900

    Initial commit

---
+++ src/main/java/com/moong/envers/RevListenerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.moong.envers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevListenerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevListenerDemoApplication.class, args);
+	}
+
+}
