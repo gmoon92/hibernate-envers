b8c4ecca13e697fc7ab872df7da2d6626c5d411c
gmoon0929 <gmoon0929@gmail.com>
2019-11-24 12:49:25 +0900

    BaseJPARepositoryTestCase 테스트 케이스 상속으로 인한 리펙토링

--- src/test/java/com/moong/envers/SpringBootH2IntegrationTest.java
+++ src/test/java/com/moong/envers/SpringBootH2IntegrationTest.java
@@ -1,6 +1,6 @@
 package com.moong.envers;
 
-import com.moong.envers.common.config.H2JPATestConfig;
+import com.moong.envers.common.config.BaseJPARepositoryTestCase;
 import com.moong.envers.common.config.H2ServerConfig;
 import com.moong.envers.member.domain.Member;
 import com.moong.envers.member.repo.MemberRepository;
@@ -12,15 +12,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
-@SpringBootTest(classes = {RevListenerDemoApplication.class, H2ServerConfig.class, H2JPATestConfig.class})
-public class SpringBootH2IntegrationTest {
+@SpringBootTest(classes = {RevListenerDemoApplication.class, H2ServerConfig.class, BaseJPARepositoryTestCase.class})
+class SpringBootH2IntegrationTest {
 
     @Autowired
     private MemberRepository memberRepository;
 
     @Test
     @Transactional
-    public void givenRepository_whenSaveAndRetrieveEntity(){
+    void givenRepository_whenSaveAndRetrieveEntity(){
         Member member = memberRepository.save(Member.builder().name("moong").build());
         Member findMember = memberRepository.findById(member.getId()).get();
 
--- src/test/java/com/moong/envers/envers/revison/RevisionTraceQueryTest.java
+++ src/test/java/com/moong/envers/envers/revison/RevisionTraceQueryTest.java
@@ -1,43 +1,36 @@
 package com.moong.envers.envers.revison;
 
-import com.moong.envers.common.config.JPAConfig;
+import com.moong.envers.common.config.BaseJPARepositoryTestCase;
 import com.moong.envers.member.domain.Member;
 import com.moong.envers.member.repo.MemberRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.envers.AuditReader;
 import org.hibernate.envers.AuditReaderFactory;
 import org.hibernate.envers.query.AuditEntity;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 @Slf4j
-@SpringBootTest
-public class RevisionTraceQueryTest {
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+class RevisionTraceQueryTest extends BaseJPARepositoryTestCase {
 
-    @PersistenceContext(name = JPAConfig.PERSISTENCE_UNIT_NAME)
-    private EntityManager em;
-
-    @Autowired
-    private MemberRepository memberRepository;
-
+    private final MemberRepository memberRepository;
     private Member admin;
 
     @BeforeEach
-    public void init() {
+    void init() {
         admin = memberRepository.findByName("admin").get();
     }
 
     @Test
+    @DisplayName("다양한 Aud 테이블 조회 방식")
     @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void getEntityAud_세가지_방식() {
+    void getEntityAud_세가지_방식() {
         Long rev = 3L;
 
         AuditReader auditReader = AuditReaderFactory.get(em);
