77b2cd863a6b25691dc375881b448314b93bc71c
gmoon92 <gmoon0929@gmail.com>
2019-12-09 14:57:56 +0900

    신청서 조회, 삭제, 저장 기능 추가 승인자 도메인 cascade 옵션 제거 @PreRemove 사용으로 fk 연관관계 끊어줌

--- src/test/java/com/moong/envers/common/config/BaseTestCase.java
+++ src/test/java/com/moong/envers/common/config/BaseTestCase.java
@@ -10,7 +10,7 @@ import static org.springframework.test.context.TestConstructor.AutowireMode.ALL;
  * @Slf4j는 애초에 @Inherited 애노테이션이 부착되어 있지 않아
  * 상속 구조에서 자식들의 클래스에서 log를 사용할 수 없다.
  * 따라서 애노테이션 대신 log 인스턴스를 명시적으로 생성하였다.
- *@author moong
+ * @author moong
  * */
 @TestConstructor(autowireMode = ALL)
 public abstract class BaseTestCase {
