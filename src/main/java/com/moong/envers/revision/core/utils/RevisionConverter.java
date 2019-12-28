20bd79cf37541b6dd81502bdfb7df634cec28ead
gmoon0929 <gmoon0929@gmail.com>
2019-12-28 15:37:31 +0900

    RevisionConvert 테스트 코드 추가

--- src/main/java/com/moong/envers/revision/core/utils/RevisionConverter.java
+++ src/main/java/com/moong/envers/revision/core/utils/RevisionConverter.java
@@ -4,6 +4,7 @@ import com.moong.envers.common.domain.BaseEntity;
 import com.moong.envers.common.vo.EntityCompareVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
+import org.springframework.util.Assert;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -20,37 +21,39 @@ public final class RevisionConverter {
      * Serializable byte array -> DataBase save
      * https://www.javaspecialists.eu/archive/Issue020.html
      * https://stackoverflow.com/questions/26901706/how-to-save-a-serialized-object-in-a-database
+     *
      * @author moong
-     * */
+     */
     public static byte[] serializedObject(Serializable entityId) {
         byte[] bytes = null;
         try (ByteArrayOutputStream bos = new ByteArrayOutputStream(); ObjectOutput out = new ObjectOutputStream(bos)) {
             out.writeObject(entityId);
             out.flush();
             bytes = bos.toByteArray();
-            log.info("serializedObject ({}) : {}", bytes.length, bytes);
         } catch (IOException e) {
             throw new RuntimeException("Entity ID type serialization is not converted to a byte array", e);
         } finally {
+            log.info("serializedObject ({}) : {}", bytes.length, bytes);
             return bytes;
         }
     }
 
     /**
-     * DeSerializable DataBase byte array -> Java Object
+     * 저장된 byte array에 해당된 Object의
+     * 필드명이 바뀌거나, 새로운 필드값에 대해선 추적 불가능.
+     * https://www.baeldung.com/junit-assert-exception
      * @author moong
-     * */
+     */
     public static Object deSerializedObject(byte[] entityId) {
-        Object deSerializedObject = null;
-        if (entityId == null)
-            return deSerializedObject;
+        Assert.notNull(entityId, "Error with entityId parameter null");
 
+        Object deSerializedObject = null;
         try (ObjectInputStream objectIn = new ObjectInputStream(new ByteArrayInputStream(entityId))) {
             deSerializedObject = objectIn.readObject();
-            log.info("deSerializedObject : {}", deSerializedObject);
         } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException("Entity ID type byte array is not converted to object", e);
         } finally {
+            log.info("deSerializedObject : {}", deSerializedObject);
             return deSerializedObject;
         }
     }
@@ -60,7 +63,6 @@ public final class RevisionConverter {
         try {
             compareVO = compareVOClass.newInstance();
         } catch (InstantiationException | IllegalAccessException e) {
-            log.error("{}", e);
             throw new RuntimeException("Does Not convert to EntityCompareVO object", e);
         }
 
